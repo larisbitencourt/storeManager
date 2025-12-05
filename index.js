fa1c5df77b6765f3960730d66fc73b56c35ae0df
Larissa Bitencourt <larissabitencourt95@gmail.com>
2025-12-05 10:29:35 -0300

    chore: criando diretórios e conexão com MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const MONGO_DB_URL = "mongodb://localhost:27017/StoreManager";
+const DB_NAME = "StoreManager";
+
+mongoose.connect(MONGO_DB_URL, {
+  dbName: DB_NAME,
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.send();
 });
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
