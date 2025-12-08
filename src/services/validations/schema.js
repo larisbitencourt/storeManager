5be7e41fe4b24b78f070d601dffeba481f41acdf
Larissa Bitencourt <larissabitencourt95@gmail.com>
2025-12-08 11:03:45 -0300

    feat: terceiro requisito finalizado, terceiro teste describe concluído

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -8,6 +8,14 @@ const saveProductsSchema = Joi.object({
   }),
 });
 
+const updateProductSchema = Joi.object({
+  name: Joi.string().min(5).required(),
+  quantity: Joi.number().integer().min(1).required().messages({
+     'number.min': '"quantity" must be larger than or equal to 1',
+  })
+})
+
 module.exports = {
   saveProductsSchema,
+  updateProductSchema,
 };
