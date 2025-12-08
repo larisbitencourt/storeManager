1b0db7086d1cbf02fdc82f92bda5d14d081866a7
Larissa Bitencourt <larissabitencourt95@gmail.com>
2025-12-08 10:23:07 -0300

    feat: segundo requisito finalizado, segundo teste describe concluído

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,10 @@ const productsController  = require('../controllers/productsController')
 
 router.post('/products', productsController.saveProducts);
 
+router.get('/products/', productsController.getAll);
+
+router.get('/products/:id', productsController.getById);
+
 
 
 module.exports = router;
